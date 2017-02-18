da8d44361ebf496626e63cd238a4e247bfbc6a8c
punit <punit@localhost>
2017-02-18 08:11:35 -0800

    Added createOperator method and

--- src/main/java/com/punit/practice/calculatorforintegers/operator/OperatorEnum.java
+++ src/main/java/com/punit/practice/calculatorforintegers/operator/OperatorEnum.java
@@ -10,5 +10,16 @@ package com.punit.practice.calculatorforintegers.operator;
  * @author poza
  */
 public enum OperatorEnum {
-    add, sub, mult, div, let
+    add("org.practice.punit.calculatorforintegers.operator.Addition"), 
+    sub("org.practice.punit.calculatorforintegers.operator.Substraction"), 
+    mult("org.practice.punit.calculatorforintegers.operator.Multiplication"), 
+    div("org.practice.punit.calculatorforintegers.operator.Division"), 
+    let("org.practice.punit.calculatorforintegers.operator.Assignment");
+    private final String className;
+    OperatorEnum(String className) {
+        this.className = className;
+    }
+    public String getClassName() {
+        return this.className;
+    }
 }
